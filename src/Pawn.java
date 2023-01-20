a9a0954cd3b98de6ab0781bb6fa09b9d62d4a61f
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-20 16:55:53 +0400

    ex3 in the process

---
+++ src/Pawn.java
@@ -0,0 +1,48 @@
+public class Pawn extends ChessPiece{
+
+    public Pawn(String color) {
+        super(color);
+    }
+    @Override
+    public String getColor() {
+        return color;
+    }
+    @Override
+    public String getSymbol() {
+        return "P";
+    }
+
+    @Override
+    public boolean canMoveToPosition(
+            ChessBoard chessBoard,
+            int line, int column,
+            int toLine, int toColumn) {
+
+        //target is on board
+        if (!(
+                0 <= line && line <= 7 &&
+                        0 <= column && column <= 7 &&
+                        0 <= toLine && toLine <= 7 &&
+                        0 <= toColumn && toColumn <= 7))
+            return false;
+
+        //not go to the same position
+        if (line == toLine && column == toColumn)
+            return false;
+
+        //can go like a PAWN
+        if (
+                        (toLine == line + 1 && toColumn == column + 2) ||
+                        (toLine == line + 2 && toColumn == column + 1) ||
+                        (toLine == line - 1 && toColumn == column + 2) ||
+                        (toLine == line - 2 && toColumn == column + 1) ||
+                        (toLine == line - 2 && toColumn == column - 1) ||
+                        (toLine == line - 1 && toColumn == column - 2) ||
+                        (toLine == line + 1 && toColumn == column - 2) ||
+                        (toLine == line + 2 && toColumn == column - 1)
+
+        ) return true;
+        else return false;
+    }
+
+}
