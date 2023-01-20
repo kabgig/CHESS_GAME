bd7389cc7f34ccd491e8ac1676381b2342b3a6d2
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-20 15:45:25 +0400

    ex2 started to copy given classes from codeboard

--- src/Horse.java
+++ src/Horse.java
@@ -6,16 +6,19 @@ public class Horse extends ChessPiece{
 
     @Override
     public String getColor() {
-        return null;
+        return color;
     }
 
     @Override
-    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+    public boolean canMoveToPosition(
+            ChessBoard chessBoard,
+            int line, int column,
+            int toLine, int toColumn) {
         return false;
     }
 
     @Override
     public String getSymbol() {
-        return null;
+        return "H";
     }
 }
